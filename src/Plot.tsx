653ccfe84deec6e9469a2614f4f3e89d4d2ca512
Frank Imhof <imhof.frank@gmail.com>
2020-10-17 17:19:45 +0200

    added colors

--- src/Plot.tsx
+++ src/Plot.tsx
@@ -3,10 +3,17 @@ import * as d3 from 'd3';
 import data from './handshakes_pretty.json';
 import {useTransition, animated} from 'react-spring';
 
-const sig=Object.entries(data)[0][0]
-const sigs=Object.entries(data)
-
 type dataEntry = [string, number];
+const sigs=Object.entries(data)
+console.log(sigs)
+//@ts-ignore
+const maxValue=1/Math.min(...sigs
+  .map((sig)=>sig[1])
+  .map(kem=>Object.values(kem))
+  .map(v=>Math.min(...v)))
+console.log(maxValue)
+//const max=Math.max(allValues);
+//console.log(max)
 type Props = {
 }
 type BarGroupProps = {
@@ -14,18 +21,8 @@ type BarGroupProps = {
   boundedHeight: number
 }
 
-type BarProps = {
-  xOffset: number,
-  width: number,
-  height: number,
-  xLabel: string,
-  yLabel: number,
-  y: number,
-}
-
 const Plot: React.FC<Props> = () =>{
   const [chosenSig, setChosenSig]=useState<string[]>([sigs[0][0], sigs[0][0]])
-  const prevChosenSig= chosenSig[1];
   //@ts-ignore
   const entriesAsList:dataEntry[]=Object.entries(data[chosenSig[0]]).sort()
   //@ts-ignore
@@ -36,9 +33,10 @@ const Plot: React.FC<Props> = () =>{
   const marginLeft=20;
   const marginRight=10;
   const marginTop=10;
-  const marginBottom=80;
+  const marginBottom=100;
   const boundedWidth=width-marginLeft-marginRight;
   const boundedHeight=height-marginTop-marginBottom;
+  const barWidth = boundedWidth/13;
   const xAccessor=(d:dataEntry)=>d[0]
   const yAccessor=(d:dataEntry)=>1/d[1]
   //@ts-ignore
@@ -49,7 +47,9 @@ const Plot: React.FC<Props> = () =>{
   const prevYScale = d3.scaleLinear().domain(d3.extent(prevEntriesAsList, yAccessor))//gives us min max
     .range([0, boundedHeight-20])
     .nice()
-
+  //@ts-ignore
+  const colorScale = d3.scaleLinear().domain([0, 0.001]).range([0.3, 0.8]).clamp(true);//more than 20ms will be red
+  //@ts-ignore
   const BarGroup: React.FC<BarGroupProps> = ({boundedHeight, boundedWidth}) =>{
     //@ts-ignore
     const transition = useTransition(
@@ -68,10 +68,14 @@ const Plot: React.FC<Props> = () =>{
     )
     //@ts-ignore
     return transition.map(({item, props, key}, index)=>
-      <>
-        <text>gugui</text>
-        <animated.rect key={key} style={{...props, fill: "red"}} x={index*40} width={20} rx={5}></animated.rect>
-      </>
+          <g transform={`translate(${index/13*boundedWidth}, 0)`} key={index}>
+            //@ts-ignore
+            <animated.rect key={key} style={{...props, fill:`${d3.interpolateReds(colorScale(yAccessor(entriesAsList[index])))}`}} width={barWidth-2} rx={5}></animated.rect>
+                //@ts-ignore
+                <animated.text y={props.y} style={{...props, transform: "translate(0px, -10px)", fontSize: "12px", fontWeight: "bold", textAnchor: "middle", fill:`${d3.interpolateReds(colorScale(yAccessor(entriesAsList[index])))}`}} x={barWidth/2}>{(yAccessor(entriesAsList[index])*1000).toString().slice(0,5)}ms</animated.text>
+                //@ts-ignore
+                <text style={{fill: "white", transformOrigin: "top left", transform: `translate(${barWidth/2}px, ${boundedHeight+10}px) rotate(-45deg)`, fontSize: "15px", fontWeight: "bold", textAnchor: "end"}}>{xAccessor(entriesAsList[index])}</text>
+              </g>
     )
   }
 
@@ -83,7 +87,7 @@ const Plot: React.FC<Props> = () =>{
           sigs.map((sig, index)=><button onClick={(e:MouseEvent)=>setChosenSig([e.target.innerHTML,...chosenSig].slice(0,2))} key={index}>{sig[0]}</button>)
         }
       </div>
-      <p style={{color: "white"}}>{chosenSig}</p>      
+      <h3 style={{color: "white"}}>{chosenSig}</h3>      
       <div className="plot">
         <svg style={{backgroundColor: "#282c34", borderRadius: "5px"}} width={width} height={height}>
           <g transform={`translate(${marginLeft}, ${marginTop})`}>
