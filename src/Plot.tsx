d08ed182fd6e8f240cc0b696a1ee3cc4d2c784b1
Frank Imhof <imhof.frank@gmail.com>
2020-10-17 15:39:07 +0200

    added animation

--- src/Plot.tsx
+++ src/Plot.tsx
@@ -24,9 +24,12 @@ type BarProps = {
 }
 
 const Plot: React.FC<Props> = () =>{
-  const [chosenSig, setChosenSig]=useState<string>(sigs[0][0])
+  const [chosenSig, setChosenSig]=useState<string[]>([sigs[0][0], sigs[0][0]])
+  const prevChosenSig= chosenSig[1];
   //@ts-ignore
-  const entriesAsList=Object.entries(data[chosenSig]).sort()
+  const entriesAsList:dataEntry[]=Object.entries(data[chosenSig[0]]).sort()
+  //@ts-ignore
+  const prevEntriesAsList:dataEntry[]=Object.entries(data[chosenSig[1]]).sort()
 
   const width=1000;
   const height=500;
@@ -36,37 +39,37 @@ const Plot: React.FC<Props> = () =>{
   const marginBottom=80;
   const boundedWidth=width-marginLeft-marginRight;
   const boundedHeight=height-marginTop-marginBottom;
-  const barWidth = boundedWidth/13-2;
   const xAccessor=(d:dataEntry)=>d[0]
-  const yAccessor=(d:dataEntry)=>d[1]
+  const yAccessor=(d:dataEntry)=>1/d[1]
   //@ts-ignore
   const yScale = d3.scaleLinear().domain(d3.extent(entriesAsList, yAccessor))//gives us min max
     .range([0, boundedHeight-20])
     .nice()
+  //@ts-ignore
+  const prevYScale = d3.scaleLinear().domain(d3.extent(prevEntriesAsList, yAccessor))//gives us min max
+    .range([0, boundedHeight-20])
+    .nice()
 
   const BarGroup: React.FC<BarGroupProps> = ({boundedHeight, boundedWidth}) =>{
-    const transitionRef = useRef();
     //@ts-ignore
     const transition = useTransition(
       //@ts-ignore
-      entriesAsList.map(d=>({height: yScale(yAccessor(d as dataEntry)), y: boundedHeight-yScale(yAccessor(d as dataEntry))})),
+      entriesAsList.map((d, index)=>({prevHeight: prevYScale(yAccessor(prevEntriesAsList[index] as dataEntry)), prevY: boundedHeight-prevYScale(yAccessor(prevEntriesAsList[index] as dataEntry)), height: yScale(yAccessor(d as dataEntry)), y: boundedHeight-yScale(yAccessor(d as dataEntry))})),
       //@ts-ignore
       (entry, index) => index,
       {
-        unique: true,
-        //@ts-ignore
-        from: ({height, y})=>({height, y}),
+        from: ({prevHeight, prevY})=>({height: prevHeight, y: prevY}),
         //@ts-ignore
         //@ts-ignore
         enter: ({height, y})=>({height, y}),
         //@ts-ignore
         update: ({height, y})=>({height, y}),
-        ref: transitionRef
       }
     )
     //@ts-ignore
     return transition.map(({item, props, key}, index)=>
       <>
+        <text>gugui</text>
         <animated.rect key={key} style={{...props, fill: "red"}} x={index*40} width={20} rx={5}></animated.rect>
       </>
     )
@@ -77,7 +80,7 @@ const Plot: React.FC<Props> = () =>{
       <div style={{display: "flex", flexDirection: "row", flexWrap: "wrap"}}>
         {
         //@ts-ignore
-        sigs.map((sig, index)=><button onClick={(e:MouseEvent)=>setChosenSig(e.target.innerHTML)} key={index}>{sig[0]}</button>)
+          sigs.map((sig, index)=><button onClick={(e:MouseEvent)=>setChosenSig([e.target.innerHTML,...chosenSig].slice(0,2))} key={index}>{sig[0]}</button>)
         }
       </div>
       <p style={{color: "white"}}>{chosenSig}</p>      
